9db94aa43eda90f64aad213e8249968d79c34f77
KARAN11700 <karansharda132@gmail.com>
2026-04-04 22:23:29 +0530

    shop project

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask
+import urllib.parse
+
+app = Flask(__name__)
+
+# 🔴 CHANGE THESE
+SHOP_WHATSAPP = "917888650410"
+GOOGLE_REVIEW = "https://www.google.co.in/maps/place/SHARDA+CLOTH+STORE/@30.3181785,76.3929729,17z/data=!4m8!3m7!1s0x3910298b7cc8435b:0x254e4dae5eddb8c4!8m2!3d30.3181785!4d76.3955478!9m1!1b1!16s%2Fg%2F11tp7d7_1x?entry=ttu&g_ep=EgoyMDI2MDQwMS4wIKXMDSoASAFQAw%3D%3D"
+
+@app.route("/")
+def home():
+    message = "Thanks for visiting Sharda Cloth Shop 😊 Please give us a review ⭐"
+
+    encoded_msg = urllib.parse.quote(message)
+
+    whatsapp_url = f"https://wa.me/{SHOP_WHATSAPP}?text={encoded_msg}"
+
+    return f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Sharda Cloth Shop</title>
+    </head>
+    <body style="text-align:center; margin-top:50px;">
+
+        <h2>Welcome to Sharda Cloth Shop 👕</h2>
+        <p>Click below to continue</p>
+
+        <br>
+
+        <a href="{whatsapp_url}" target="_blank">
+            <button style="padding:10px 20px;">Send WhatsApp Message</button>
+        </a>
+
+        <br><br>
+
+        <a href="{GOOGLE_REVIEW}" target="_blank">
+            <button style="padding:10px 20px;">Give Google Review ⭐</button>
+        </a>
+
+    </body>
+    </html>
+    """
+
+if __name__ == "__main__":
+    app.run(debug=True)
